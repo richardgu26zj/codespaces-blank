c6ec1b56ebe58760cf8fe148fa0a15edad36e63e
richardgu26zj <70470386+richardgu26zj@users.noreply.github.com>
2026-01-04 14:10:36 +0000

    modified

--- Bayesian Macroeconometrics/src/Bayesian_Simple_LR.jl
+++ Bayesian Macroeconometrics/src/Bayesian_Simple_LR.jl
@@ -9,14 +9,16 @@ date = range(1959.25, stop = 2025.25, length = length(data));
 # Initialize plotting of data
 plot(date, data, 
      color =:blue,
-     lw = 3; 
+     lw = 2.5; 
      grid =:both,
      gridalpha = 0.15,
      legend = false,
-     xlabel = L"\text{Quarters} (Q)",
-     ylabel = L"\text{Inflation Rate} (\%)",
+     xlabel = L"\text{Quarters (Q)}",
+     ylabel = L"\text{Inflation Rate (\%)}",
      title = L"\text{US Inflation Rate (PCE)}",
-     titlefont = font(16, :bold),
+     titlefontsize = 16,
+     xlims = (1959.00, 2025.50),
+     tickfontfamily = "Computer Modern"
      )
 
 savefig("figures/inflation.pdf")
